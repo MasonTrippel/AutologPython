eebd76bc9d6d2573f16cbea11e885940d67494b5
Mason <minisyndicate89@gmail.com>
2024-04-12 22:08:01 -0400

    Basic GUI

--- App.py
+++ App.py
@@ -1,12 +1,24 @@
+import tkinter as tk
 from tkinter import *
+import mysql.connector
 
-class Application:
-    def __init__(self, master=None):
-        self.master = master
-        self.label = Label(text="Hello Tkinter")
-        self.label.pack()
+root = tk.Tk()
+root.title('LOL Auto Log')
+root.geometry('')
 
-if __name__ == "__main__":
-    root = Tk()
-    app = Application(master=root)
-    root.mainloop()
+listbox = Listbox(root, font="Railway", bg="#C10D0D", fg="black", height=3, width=15)
+listbox.grid(column=0, row=2)
+
+
+accounts = ["Trippelindy", "Trippy", "SKTDopa"]
+for account in accounts:
+    listbox.insert(END, account)
+
+
+def on_select(event):
+    index = event.widget.curselection()[0]
+    
+
+    
+
+listbox.bind('<<ListboxSelect>>', on_select)   
